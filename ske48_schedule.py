45d22f5c07a61172dfbe0f9dd4e1d162e6488fb4
Juichi Hirao <hirao@theta-inc.co.jp>
2019-11-17 22:40:43 +0900

    ske48 不要コードの削除

--- ske48_schedule.py
+++ ske48_schedule.py
@@ -16,13 +16,9 @@ class Ske48Schedule:
 
     def __init__(self):
 
-        self.start_year = 2017
+        self.start_year = 2019
         self.start_month = 10
-        # self.conn = self.__get_conn()
-        # self.cursor = self.conn.cursor()
         self.base_url = 'http://www.ske48.co.jp/schedule/'
-        # self.main_url = 'http://www.ske48.co.jp/schedule/calendar.php?y=2018&m=10'
-        # http://www.ske48.co.jp/schedule/?id=1569313609
         self.detail_other_list = []
         self.detail_none = []
 
@@ -30,20 +26,18 @@ class Ske48Schedule:
 
     def get_one_month_no_detail(self, year, month):
 
-        # self.main_url = 'http://www.ske48.co.jp/schedule/calendar.php?y=2018&m=10'
+        # http://www.ske48.co.jp/schedule/calendar.php?y=2018&m=10
         one_month_url = os.path.join(self.base_url, 'calendar.php?y={}&m={}'.format(year, month))
         # print(one_month_url)
         with urllib.request.urlopen(one_month_url) as response:
             html = response.read()
             html_soup = BeautifulSoup(html, "html.parser")
             # print(html_soup)
-            # stage_list = html_soup.find_all('li', class_='stage')
             td_list = html_soup.find_all('tr')
             idx = 0
             for td in td_list:
 
                 th_data = td.find('th')
-                # m_day = re.search('<th>(?P<day>[0-9]{1,2})日\([月火水木金土日]\)</th>', str(th_data))
                 m_day = re.search('(<th>|<th class.*>)(?P<day>[0-9]{1,2})日', str(th_data))
                 day = ''
                 if m_day:
@@ -63,7 +57,7 @@ class Ske48Schedule:
                         stage_url = a_link.attrs['href']
 
                     # print(stage_name_text)
-                    the_date, title = self.__parse_title_no_detail(stage_name_text, year, month, day_str)
+                    the_date, title = self.__parse_title(stage_name_text, year, month, day_str)
                     if the_date is None:
                         one_schedule_url = '{}{}'.format(self.base_url, stage_url.replace('./', ''))
                         the_date = datetime.strptime('{}/{}/{}'.format(year, month, day_str), '%Y/%m/%d')
@@ -94,51 +88,8 @@ class Ske48Schedule:
                     if not self.db.is_exist(data.theDate, data.groupName):
                         self.db.export(data)
 
-
-                '''
-                if 'href' in a_link.attrs:
-                    stage_url = a_link.attrs['href']
-                print('{} {}'.format(stage, stage_url))
-
-                one_schedule_url = '{}{}'.format(self.base_url, stage_url.replace('./', ''))
-                print(one_schedule_url)
-
-                with urllib.request.urlopen(one_schedule_url) as response:
-                    detail_html = response.read()
-                    detail_html_soup = BeautifulSoup(detail_html, "html.parser")
-                    detail = detail_html_soup.find('div', class_='detail')
-
-                    if detail is None:
-                        self.detail_none.append(str(stage))
-                        continue
-
-                    the_date, title = self.__parse_title(detail.find('h3'))
-                    # if len(detail_date) > 0:
-                    #     theDate = datetime.strptime('{} {}:00'.format(detail_date, time_str), '%Y.%m.%d %H:%M:%S')
-
-                    member, time_str = self.__parse_detail(str(detail)) # detail.textだと、BRタグが抜けてしまう
-                    # if len(time_str) > 0:
-                    #     theDate = datetime.strptime('{} {}:00'.format(detail_date, time_str), '%Y.%m.%d %H:%M:%S')
-                    # else:
-                    #     theDate = datetime.strptime('{}'.format(detail_date), '%Y.%m.%d')
-                    # detail_text = str(detail)
-                    print('  one_schedule [{}] {}'.format(the_date, title))
-                    print('    {}'.format(member))
-
-                # if idx > 2:
-                #     break
-                idx = idx + 1
-                '''
-            # stage_list = html_soup.find_all('dt', class_='stage')
-            # for stage in stage_list:
-            #     span_date = stage.find('span', class_='new')
-            #     print('{} {}'.format(stage, span_date))
-
-            # entrys = html_soup.find_all('div', class_='archive-content')
-
     def __get_member(self, url):
         one_schedule_url = '{}{}'.format(self.base_url, url.replace('./', ''))
-        # print(one_schedule_url)
 
         with urllib.request.urlopen(one_schedule_url) as response:
             detail_html = response.read()
@@ -153,68 +104,10 @@ class Ske48Schedule:
 
         return member
 
-    def get_one_month(self, year, month):
-
-        # self.main_url = 'http://www.ske48.co.jp/schedule/calendar.php?y=2018&m=10'
-        one_month_url = os.path.join(self.base_url, 'calendar.php?y={}&m={}'.format(year, month))
-        print(one_month_url)
-        print('http://www.ske48.co.jp/schedule/calendar.php?y=2019&m=10')
-        with urllib.request.urlopen(one_month_url) as response:
-            html = response.read()
-            html_soup = BeautifulSoup(html, "html.parser")
-            # print(html_soup)
-            stage_list = html_soup.find_all('li', class_='stage')
-            idx = 0
-            for stage in stage_list:
-                a_link = stage.find('a')
-                if '劇場休館日' == stage.text:
-                    continue
-
-                if 'href' in a_link.attrs:
-                    stage_url = a_link.attrs['href']
-                print('{} {}'.format(stage, stage_url))
-
-                one_schedule_url = '{}{}'.format(self.base_url, stage_url.replace('./', ''))
-                print(one_schedule_url)
-                with urllib.request.urlopen(one_schedule_url) as response:
-                    detail_html = response.read()
-                    detail_html_soup = BeautifulSoup(detail_html, "html.parser")
-                    detail = detail_html_soup.find('div', class_='detail')
-
-                    if detail is None:
-                        self.detail_none.append(str(stage))
-                        continue
-
-                    the_date, title = self.__parse_title(detail.find('h3'))
-                    # if len(detail_date) > 0:
-                    #     theDate = datetime.strptime('{} {}:00'.format(detail_date, time_str), '%Y.%m.%d %H:%M:%S')
-
-                    member, time_str = self.__parse_detail(str(detail)) # detail.textだと、BRタグが抜けてしまう
-                    # if len(time_str) > 0:
-                    #     theDate = datetime.strptime('{} {}:00'.format(detail_date, time_str), '%Y.%m.%d %H:%M:%S')
-                    # else:
-                    #     theDate = datetime.strptime('{}'.format(detail_date), '%Y.%m.%d')
-                    # detail_text = str(detail)
-                    print('  one_schedule [{}] {}'.format(the_date, title))
-                    print('    {}'.format(member))
-
-                # if idx > 2:
-                #     break
-                idx = idx + 1
-            # stage_list = html_soup.find_all('dt', class_='stage')
-            # for stage in stage_list:
-            #     span_date = stage.find('span', class_='new')
-            #     print('{} {}'.format(stage, span_date))
-
-            # entrys = html_soup.find_all('div', class_='archive-content')
-
-    def __parse_title_no_detail(self, title_text, year, month, day):
+    def __parse_title(self, title_text, year, month, day):
 
         m_time = re.search('[' + re.escape('（(') + '](?P<time_str>[0-2][0-9]:[0-6][0-9])開演[' + re.escape('）)') + ']', title_text)
-        # print(title_text)
-        # m_time = re.search('(?P<time_str>[0-2][0-9]:[0-6][0-9])開演', title_text)
         if m_time:
-            # time_str = m_time.group('time_str').replace('24:', '00:')
             time_str = m_time.group('time_str')
             if re.search('2[456]:', time_str):
                 time_str = time_str.replace('24:', '00:').replace('25:', '01:').replace('26:', '02:')
@@ -228,28 +121,6 @@ class Ske48Schedule:
         else:
             title = ''
             the_date = None
-
-        return the_date, title
-
-    def __parse_title(self, h3_title):
-
-        if h3_title is None:
-            return '', ''
-
-        title = h3_title.text
-
-        m_detail_date = re.search('20[0123][0-9]\.[01][0-9]\.[0-3][0-9]', h3_title.text)
-        if m_detail_date:
-            date_str = m_detail_date.group()
-            title = title.replace(date_str, '')
-
-        m_time = re.search('(?P<time_str>[0-2][0-9]:[0-6][0-9])開演<br/><br/>', h3_title.text)
-        if m_time:
-            time_str = m_time.group('time_str')
-            replace_time_str = m_time.group()
-            title = title.replace(replace_time_str, '')
-
-        the_date = datetime.strptime('{} {}:00'.format(date_str, time_str), '%Y.%m.%d %H:%M:%S')
 
         return the_date, title
 
@@ -288,11 +159,7 @@ class Ske48Schedule:
             end_month = now_date.month if year == now_date.year else 13
             for month in range(start_month, end_month):
                 print('{}-{:0>2}'.format(year, month))
-                # self.get_one_month(year, month)
                 self.get_one_month_no_detail(year, month)
-
-                # if month == 12:
-                #     return
 
         for detail_other in self.detail_other_list:
             print(detail_other)
